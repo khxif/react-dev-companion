a1c5cf6ae71162bfadf61ebaef37c9b44e4624b1
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-08-22 19:39:46 +0530

    fix: imports fixed

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       fileName: format => `react-dev-companion.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
