44968a64c5e384ede1640a2462ad254a4626d7cf
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-08-24 19:36:13 +0530

    feat: added cat waking up animation

--- src/companion.tsx
+++ src/companion.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as React from 'react';
+import CatWakeup from './assets/cat-wakeup.mp4';
 import CatWalking from './assets/cat-walk.mp4';
 import { AppContextProvider, useAppContext } from './contexts/app-context';
 import { useTabActiveTime } from './hooks/use-tab-active-time';
@@ -8,18 +9,15 @@ import { formatMsToMinSec } from './utils/lib';
 
 interface CompanionProps {
   direction?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
-  theme?: 'light' | 'dark';
   resetOnClick?: boolean;
 }
 
-function Companion({
-  direction = 'bottom-right',
-  theme = 'dark',
-  resetOnClick = false,
-}: CompanionProps) {
+function Companion({ direction = 'bottom-right', resetOnClick = false }: CompanionProps) {
   const { activeTime } = useTabActiveTime();
   const { resetActiveTime } = useAppContext();
   const formattedTime = formatMsToMinSec(activeTime);
+
+  const isCatWakingUp = activeTime < 5000;
 
   return (
     <div
@@ -32,20 +30,24 @@ function Companion({
     >
       <button
         {...(resetOnClick && { onClick: resetActiveTime })}
-        className={`p-2 rounded-full size-fit button peer cursor-pointer z-10
-          ${theme === 'light' ? 'bg-white text-black' : 'text-white bg-black'}
-          `}
+        className="p-2 rounded-full size-fit button peer cursor-pointer z-10 text-white bg-black "
       >
-        <video loop className="size-10" autoPlay muted playsInline>
-          <source src={CatWalking} type="video/mp4" />
+        <video
+          className="size-10"
+          autoPlay
+          muted
+          playsInline
+          key={isCatWakingUp ? 'wakeup' : 'walk'} // force remount
+          loop={!isCatWakingUp}
+        >
+          <source src={isCatWakingUp ? CatWakeup : CatWalking} type="video/mp4" />
         </video>
       </button>
 
       <span
         className={`w-28 px-2 py-1 rounded-md absolute text-center mb-0.5
-       text-xs hidden peer-hover:block bottom-full  
-        ${theme === 'light' ? 'bg-white text-black' : 'text-gray-200 bg-black'}
-        ${direction === 'top-left' ? '-left-5' : ''} 
+       text-xs hidden peer-hover:block bottom-full text-gray-200 bg-black
+        ${direction === 'top-left' ? '-left-5' : ''}
         ${direction === 'top-right' ? '-right-6' : ''}
         ${direction === 'bottom-left' ? '-left-5' : ''}
         ${direction === 'bottom-right' ? '-right-6' : ''}
